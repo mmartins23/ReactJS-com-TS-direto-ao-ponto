4a4f006bc768534720d9442767ca6dfa84221973
mmartins23 <marlon-martins@outlook.com>
2024-05-07 16:20:24 +0200

    58. Tipos básicos

---
+++ Section 11- Fundamentos TypeScript/dist/types.js
@@ -0,0 +1,28 @@
+// Number
+let age = 30;
+let price = 9.99;
+console.log(typeof age); // Output: "number"
+// String 
+let fullName = "John Doe";
+let greeting = `Hello, ${fullName}!`;
+console.log(typeof price); // Output: "number"
+// Boolean
+let isLogged = true;
+let hasError = false;
+console.log(typeof isLogged); // Output: "boolean"
+// Null and Undefined
+let emptyValue = null;
+let notDefined = undefined;
+console.log(typeof emptyValue); // Output: "object"
+console.log(typeof notDefined); // Output: "undefined"
+// Object
+let person = { name: "Alice", age: 25 };
+let car = { brand: "Toyota", model: "Corolla" };
+console.log(typeof person); // Output: "object"
+// Array
+let numbers = [1, 2, 3, 4, 5];
+let fruits = ["apple", "banana", "orange"];
+console.log(typeof numbers); // Output: "object"
+// Tuple
+let employee = ["John", 30]; // tuple [string, number]
+console.log(typeof employee); // Output: "object"
---
+++ Section 11- Fundamentos TypeScript/src/types.ts
@@ -0,0 +1,37 @@
+// Number
+let age: number = 30;
+let price: number = 9.99;
+console.log(typeof age); // Output: "number"
+
+// String 
+let fullName: string = "John Doe";
+let greeting: string = `Hello, ${fullName}!`;
+console.log(typeof price); // Output: "number"
+
+// Boolean
+let isLogged: boolean = true;
+let hasError: boolean = false;
+console.log(typeof isLogged); // Output: "boolean"
+
+// Null and Undefined
+let emptyValue: null = null;
+let notDefined: undefined = undefined;
+console.log(typeof emptyValue); // Output: "object"
+console.log(typeof notDefined); // Output: "undefined"
+
+
+// Object
+let person: object = { name: "Alice", age: 25 };
+let car: object = { brand: "Toyota", model: "Corolla" };
+console.log(typeof person); // Output: "object"
+
+// Array
+let numbers: number[] = [1, 2, 3, 4, 5];
+let fruits: string[] = ["apple", "banana", "orange"];
+console.log(typeof numbers); // Output: "object"
+
+
+// Tuple
+let employee: [string, number] = ["John", 30]; // tuple [string, number]
+console.log(typeof employee); // Output: "object"
+
