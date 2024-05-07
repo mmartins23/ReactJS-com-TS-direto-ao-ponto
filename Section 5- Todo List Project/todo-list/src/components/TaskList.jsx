cb887b18f59258bd5b37ee8a1f8197425177c588
mmartins23 <marlon-martins@outlook.com>
2024-05-07 14:12:28 +0200

    27. Mensagem Alerta

--- Section 5- Todo List Project/todo-list/src/components/TaskList.jsx
+++ Section 5- Todo List Project/todo-list/src/components/TaskList.jsx
@@ -1,6 +1,10 @@
 import Task from "./Task";
 
 const TaskList = ({ tasks, onDeleteTask, onToggleTask }) => {
+
+    if (tasks.length === 0) {
+        return <p>There are no tasks!</p>
+    }
     return (
         <ul>
             {tasks.map(task => (
