a70a75414542960f363637180cc4305a34413cb6
mmartins23 <marlon-martins@outlook.com>
2024-05-07 08:11:17 +0200

    25. Iniciando o projeto

---
+++ Section 5- Todo List Project/todo-list/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import viteLogo from '/vite.svg';
+
+
+function App() {
+
+  return (
+    <>
+    <h2>Starting project</h2>
+    </>
+  )
+}
+
+export default App
