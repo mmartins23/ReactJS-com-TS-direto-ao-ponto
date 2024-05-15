5390c77e4d5ec47cbe213604fe2fbeb9599933b5
mmartins23 <marlon-martins@outlook.com>
2024-05-15 18:30:36 +0200

    47- Estilos iniciais

--- Section 9- Projeto Album Fotos/photos-app/src/components/SearchBar.jsx
+++ Section 9- Projeto Album Fotos/photos-app/src/components/SearchBar.jsx
@@ -1,6 +1,23 @@
 const SearchBar = () => {
+  const categories = [
+    "Nature",
+    "People",
+    "Technology",
+    "Animals",
+    "Food",
+    "Sports"
+  ]
+
   return (
-    <div>SearchBar</div>
+    <div className="search-bar">
+      <input type="text" placeholder="Search" />
+      <button>Search</button>
+      <select>
+        {categories.map(category => (
+          <option key={category} value={category}>{category}</option>
+        ))}
+      </select>
+    </div>
   )
 }
 
