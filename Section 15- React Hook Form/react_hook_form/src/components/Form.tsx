c4fc91c9337b0ab95b7ef99623435783dd6b3d4a
mmartins23 <marlon-martins@outlook.com>
2024-06-04 16:13:11 +0200

    98. Iniciando as validações

--- Section 15- React Hook Form/react_hook_form/src/components/Form.tsx
+++ Section 15- React Hook Form/react_hook_form/src/components/Form.tsx
@@ -1,6 +1,22 @@
+import { FormSchema } from "../schema/formSchema";
+import { useForms } from "../hooks/UseForms";
+
 const Form = () => {
+  const { register, handleSubmit, errors } = useForms();
+
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  }
+
   return (
-    <>Form</>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input type="text" id="name" placeholder="Insert your name" {...register("name")} />
+      {errors.name && <small>{errors.name.message}</small>}
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
   )
 }
 
